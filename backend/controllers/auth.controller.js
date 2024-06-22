dffb440c37d26e676bca1c2b1dd6fdcdfe8027e1
Milorad Jekic <moonbyt3@gmail.com>
2024-06-22 14:32:30 +0200

    Adds try catch block to login controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,27 +2,34 @@ import User from '../models/user.model.js';
 import generateToken from '../utils/generateToken.js';
 
 export const login = async (req, res) => {
-	const { username, password } = req.body;
+	try {
+		const { username, password } = req.body;
 
-	if (!username || !password) {
-		return res
-			.status(400)
-			.json({ error: 'username or password not supplied' });
-	}
+		if (!username || !password) {
+			return res
+				.status(400)
+				.json({ error: 'username or password are not supplied' });
+		}
 
-	const user = await User.findOne({ userName: username });
-	const isValidUser = await user.comparePassword(password);
+		const user = await User.findOne({ userName: username });
+		const isValidUser = await user.comparePassword(password);
 
-	if (isValidUser) {
-		return res.status(200).json({
-			message: 'Success on login!',
-		});
-	} else {
-		return res.status(400).json({
-			message: 'User with those cardentials is not found.',
-		});
+		if (isValidUser) {
+			return res.status(200).json({
+				message: 'Success on login!',
+			});
+		} else {
+			return res.status(400).json({
+				message: 'User with those cardentials is not found.',
+			});
+		}
+	} catch (error) {
+		console.log('Error in login controller', error.message);
+		res.status(400).json({ error: 'Internal server error.' });
 	}
 };
+
+export const logout = async (req, res) => {};
 
 export const signUp = async (req, res) => {
 	try {
