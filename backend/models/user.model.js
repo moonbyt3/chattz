d89b9bd1c2a43a778683252bfa4802dbedc93849
Milorad Jekic <moonbyt3@gmail.com>
2024-06-22 03:28:35 +0200

    Adds password hashing middleware

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
 
 const SALT_WORK_FACTOR = 10;
@@ -27,6 +28,32 @@ const UserSchema = new mongoose.Schema({
 		default: '',
 	},
 });
+
+UserSchema.pre('save', function (next) {
+	var user = this;
+
+	// only hash the password if it has been modified (or is new)
+	if (!user.isModified('password')) return next();
+
+	// generate a salt
+	bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+		if (err) return next(err);
+
+		// hash the password using our new salt
+		bcrypt.hash(user.password, salt, function (err, hash) {
+			if (err) return next(err);
+
+			// override the cleartext password with the hashed one
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+UserSchema.methods.comparePassword = async function (password) {
+	console.log('Comparing passwords: ', password, this.password);
+	return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', UserSchema);
 
