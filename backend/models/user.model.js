e1428883a718135eb323684e3b625fb25d3db358
Milorad Jekic <moonbyt3@gmail.com>
2024-06-22 17:55:40 +0200

    Adds timestamp to users model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,31 +3,36 @@ import mongoose from 'mongoose';
 
 const SALT_WORK_FACTOR = 10;
 
-const UserSchema = new mongoose.Schema({
-	fullName: {
-		type: String,
-		required: true,
+const UserSchema = new mongoose.Schema(
+	{
+		fullName: {
+			type: String,
+			required: true,
+		},
+		userName: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			minlength: 6,
+		},
+		gender: {
+			type: String,
+			required: true,
+			enum: ['male', 'female'],
+		},
+		profileImage: {
+			type: String,
+			default: '',
+		},
+
+		// createdAt, updatedAt
 	},
-	userName: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-		minlength: 6,
-	},
-	gender: {
-		type: String,
-		required: true,
-		enum: ['male', 'female'],
-	},
-	profileImage: {
-		type: String,
-		default: '',
-	},
-});
+	{ timestamps: true }
+);
 
 UserSchema.pre('save', function (next) {
 	var user = this;
